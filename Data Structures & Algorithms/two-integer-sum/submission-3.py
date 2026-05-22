820d19eb16064cc02cdcc3cbb3bc0053935deff1
Seroper-real <hyperapp@gmail.com>
2026-05-22 15:11:17 +0200

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}        
+        for i,val in enumerate(nums):
+            check = target - val
+            if check in mp:
+                return [mp[check],i]
+            else:
+                mp[val] = i
