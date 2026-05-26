15aae523cd13cefde6e21d8d1f5024a059998996
Seroper-real <hyperapp@gmail.com>
2026-05-26 21:57:35 +0200

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        store = defaultdict(set)
+        res_store = set()
+        for i,num in enumerate(nums):
+            store[num].add(i)
+        #print(store)
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                search = - nums[i] - nums[j]
+                #print(f"{nums[i]},{nums[j]}, s:{search}")
+                if search in store:
+                    #print(f"found:{store[search]}")
+                    count = 0
+                    if i in store[search]: count+=1
+                    if j in store[search]: count+=1
+                    if len(store[search]) > count:
+                        trio = tuple(sorted([nums[i],nums[j],search]))
+                        res_store.add(trio)
+        return list(res_store)
