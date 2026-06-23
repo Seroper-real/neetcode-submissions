1c250056185c2cb48e093981d9293faf325f4f18
Seroper-real <hyperapp@gmail.com>
2026-06-23 22:15:40 +0200

    Add: copy-linked-list-with-random-pointer - submission-1

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-1.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head == None: return None
+        store = {}
+        node = head
+        new_head = None
+        while node:
+            if node not in store:
+                new_node = Node(node.val,None,None)
+                store[node] = new_node
+            else: new_node = store.get(node)
+            new_node.random = self.get_child(store,node,node.random)
+            new_node.next = self.get_child(store,node,node.next)
+            if new_head == None: new_head = new_node
+            node = node.next
+        return new_head
+
+    def get_child(self, store: Dict[Node, Node], node: 'Optional[Node]', child: 'Optional[Node]') -> 'Optional[Node]':
+        if child:
+            if child not in store:
+                new_child = Node(child.val,None,None)
+                store[child] = new_child
+            else: new_child = store[child]
+            return new_child
+        return None
+
+            
