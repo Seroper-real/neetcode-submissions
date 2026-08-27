fe4ec49f15ec19180877d6aa6f8d49d170dca205
Seroper-real <hyperapp@gmail.com>
2026-08-27 10:16:22 +0200

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        if len(digits) == 0: return []
+        
+        mapping = {
+            '2' : ['a','b','c'],
+            '3' : ['d','e','f'],
+            '4' : ['g','h','i'],
+            '5' : ['j','k','l'],
+            '6' : ['m','n','o'],
+            '7' : ['p','q','r','s'],
+            '8' : ['t','u','v'],
+            '9' : ['w','x','y','z'],
+        }
+        
+        res = []
+        
+        def generate(dindex: int, partial: str):
+            if dindex >= len(digits):
+                res.append(partial)
+                return
+            
+            digit = digits[dindex]
+            chars = mapping[digit]
+            
+            for c in chars:
+                generate(dindex+1,partial+c)
+        
+        generate(0,"")
+        return res
+            
+            
